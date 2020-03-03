21856ab2174ba914818a9d82ed91c3ed1d26aaeb
Pieter Huycke <24294920+phuycke@users.noreply.github.com>
2020-03-03 16:00:53 +0100

    refactor code

--- Code/problem_01.py
+++ Code/problem_01.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# Problem setting
+
 """
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+@author: Pieter Huycke
+email:   pieter.huycke@ugent.be
+GitHub:  phuycke
 """
 
-def sumDivisors(number):
-    """Find the sum of number below n that are divisible by 3 and 5."""
 
-    sum = 0
-    for n in range(number):
-        if (n % 3 == 0) or (n % 5 == 0):
-            sum = sum + n
+#%%
 
-    return sum
 
-solution = sumDivisors(1000)
-print("The sum equals: %d." %(solution))
+def sumDivisors(limit : int) -> int:
+    """
+    Find the sum of all number below the limit which are divisible by 3 or 5
+
+    Parameters
+    ----------
+    limit : int
+        The upper limit for our function.
+
+    Returns
+    -------
+    sum : int
+        The sum of numbers below limit that are divisible by 3 or 5.
+
+    """
+
+    return sum(set(list(range(0, limit, 3)) + list(range(0, limit, 5))))
+
+
+#%%
+    
+print("The sum equals: %d." %(sumDivisors(limit = 1000)))
